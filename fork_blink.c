3bc57ab6a97e650022213991cb07d595938c68ff
tbrintet <122631968+tbrintet@users.noreply.github.com>
2024-03-04 17:00:02 +0100

    Add execve logging capabilities

--- fork_blink.c
+++ fork_blink.c
@@ -2,6 +2,7 @@
 #include <linux/leds.h>
 #include <linux/slab.h>
 #include <linux/fs.h>
+#include <linux/uaccess.h>
 
 DEFINE_LED_TRIGGER(fork_blink);
 
@@ -12,6 +13,7 @@ MODULE_PARM_DESC(addr, "Address of the `sys_call_table` symbol");
 typedef asmlinkage long (*sys_call_ptr_t)(const struct pt_regs *);
 static sys_call_ptr_t *sys_call_table_ptr;
 sys_call_ptr_t real_clone;
+sys_call_ptr_t real_execve;
 
 extern unsigned long __force_order;
 
@@ -45,6 +47,17 @@ static long my_clone(const struct pt_regs *pr)
 	return real_clone(pr);
 }
 
+static long my_execve(const struct pt_regs *pr)
+{
+	char buf[256];
+	if (copy_from_user(buf, (char *)pr->di, sizeof(buf)) == 0) {
+		pr_info("execve %s\n", buf);
+	} else {
+		pr_info("copy_from_user failed\n");
+	}
+	return real_execve(pr);
+}
+
 
 static int __init fork_blink_init(void)
 {
@@ -56,12 +69,14 @@ static int __init fork_blink_init(void)
 	sys_call_table_ptr = (sys_call_ptr_t *)addr;
 
 	real_clone = sys_call_table_ptr[__NR_clone];
+	real_execve = sys_call_table_ptr[__NR_execve];
 
 	// Temporarily disable write protection
 	force_write_cr0(read_cr0() & (~0x10000));
 
 	// Overwrite the syscall table entry
 	sys_call_table_ptr[__NR_clone] = my_clone;
+	sys_call_table_ptr[__NR_execve] = my_execve;
 
 	// Re-enable write protection
 	force_write_cr0(read_cr0() | 0x10000);
@@ -83,6 +98,8 @@ static void __exit fork_blink_exit(void)
 
 	// Overwrite the syscall table entry
 	sys_call_table_ptr[__NR_clone] = real_clone;
+	sys_call_table_ptr[__NR_execve] = real_execve;
+
 
 	// Re-enable write protection
 	force_write_cr0(read_cr0() | 0x10000);
