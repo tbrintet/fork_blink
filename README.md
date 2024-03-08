ce5ee756a125e10261cf4fecd157d4f77890ff36
tbrintet <122631968+tbrintet@users.noreply.github.com>
2024-03-08 10:05:58 +0100

    Add execve monitoring

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # fork_blink
-Linux Kernel Module blinking leds each time the fork system call is used.
+Linux Kernel Module intercepting clone and execve system calls.
+
+System call arguments are logged to the kernel log buffer with printk,
+which gives you the ability to monitor process execution on your machine.
+
+To get a visual indication of the number of processes being launched, you
+can configure a led to blink each time the clone system call is used.
 
 # Compilation
 
@@ -15,16 +21,25 @@ make
 sudo insmod fork_blink.ko addr=0x$(sudo grep -w 'sys_call_table' /proc/kallsyms | cut -d' ' -f1)
 ```
 
-After module load:
+**After module load**:
 
 Choose the LED in the `/sys/class/leds/` directory. Check that it can actually
 be controlled by `echo`'ing a few `0` or `1` values to its `brightness` file.
-Finally, assign the `fork_blink` LED trigger with:
+Finally, **in a root shell** (sudo does not work well with bash redirections),
+assign the `fork_blink` LED trigger with:
 ```
-# echo fork_blink > /sys/class/leds/$YOUR_LED_HERE/trigger
+echo fork_blink > /sys/class/leds/$YOUR_LED_HERE/trigger
 ```
 
-To remove the module, use
+Configuring your led is not mandatory if you are only interested in the logs.
+
+Use the following command to read the kernel ring buffer and access the logs:
+```
+sudo dmesg
+```
+
+# Removal
+To unload the module, use
 ```bash
 sudo rmmod fork_blink
 ```
--- fork_blink.c
+++ fork_blink.c
@@ -3,6 +3,9 @@
 #include <linux/slab.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
+#include <linux/console.h>
+
+#define MAX_ARG_STRINGS 0x7FFFFFFF
 
 DEFINE_LED_TRIGGER(fork_blink);
 
@@ -22,42 +25,93 @@ static inline void force_write_cr0(unsigned long val)
 	asm volatile("mov %0,%%cr0" : : "r"(val), "m"(__force_order));
 }
 
-static void print_exe_file(void)
+static char *get_current_exe_file_string(char *buffer, size_t length)
 {
-	struct mm_struct * mm = current->mm;
+	if (!buffer)
+		return NULL;
+
+	struct mm_struct *mm = current->mm;
+
 	if (!mm)
-		return;
+		return NULL;
+
+	char *p = NULL;
+
 	down_read(&mm->mmap_lock);
 	if (mm->exe_file) {
-		char * pathname = kmalloc(PATH_MAX, GFP_NOWAIT);
-		if (pathname) {
-			char * p = d_path(&mm->exe_file->f_path, pathname, PATH_MAX);
-			pr_info("clone syscall initiated by %s\n", p);
-			kfree(pathname);
-		}
+		p = d_path(&mm->exe_file->f_path, buffer, length);
 	}
 	up_read(&mm->mmap_lock);
+
+	if (IS_ERR(p))
+		return NULL;
+
+	return p;
+}
+
+static void print_clone(void)
+{
+	char *pathname = kmalloc(PATH_MAX, GFP_KERNEL);
+	if (pathname) {
+		char *p = get_current_exe_file_string(pathname, PATH_MAX);
+		if (p)
+			pr_info("clone syscall initiated by %s\n", p);
+		kfree(pathname);
+	}
+}
+
+static void print_execve(const struct pt_regs *pr)
+{
+	char *buf = kmalloc(2 * PATH_MAX, GFP_KERNEL);
+
+	if (!buf)
+		return;
+
+	char *original_exe_file = get_current_exe_file_string(buf, PATH_MAX);
+
+	if (!original_exe_file)
+		goto out;
+
+	if (strncpy_from_user(buf + PATH_MAX, (char *)pr->di, PATH_MAX) < 0)
+		goto out;
+
+	console_lock();
+	pr_info("%s ->[execve]-> %s\n", original_exe_file, buf + PATH_MAX);
+
+	char __user **argv = (char **)pr->si;
+
+	char __user *p = NULL;
+
+	for (int i = 0; i < MAX_ARG_STRINGS; i++) {
+		if (get_user(p, argv + i))
+			break;
+
+		if (!p)
+			break;
+
+		if (strncpy_from_user(buf, p, 2 * PATH_MAX) < 0)
+			break;
+
+		pr_info("argv[%d]: %s\n", i, buf);
+	}
+	console_unlock();
+out:
+	kfree(buf);
 }
 
 static long my_clone(const struct pt_regs *pr)
 {
 	static int state = 0;
-	print_exe_file();
+	print_clone();
 	led_trigger_event(fork_blink, (state ^= 1));
 	return real_clone(pr);
 }
 
 static long my_execve(const struct pt_regs *pr)
 {
-	char buf[256];
-	if (copy_from_user(buf, (char *)pr->di, sizeof(buf)) == 0) {
-		pr_info("execve %s\n", buf);
-	} else {
-		pr_info("copy_from_user failed\n");
-	}
+	print_execve(pr);
 	return real_execve(pr);
 }
-
 
 static int __init fork_blink_init(void)
 {
@@ -100,7 +154,6 @@ static void __exit fork_blink_exit(void)
 	sys_call_table_ptr[__NR_clone] = real_clone;
 	sys_call_table_ptr[__NR_execve] = real_execve;
 
-
 	// Re-enable write protection
 	force_write_cr0(read_cr0() | 0x10000);
 
@@ -111,5 +164,5 @@ module_init(fork_blink_init);
 module_exit(fork_blink_exit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Thibaut Brintet");
-MODULE_DESCRIPTION(
-	"Module overwriting the clone system call to blink leds each time it is called.");
+MODULE_DESCRIPTION("Module overwriting the clone system call to blink leds "
+		   "each time it is called.");
